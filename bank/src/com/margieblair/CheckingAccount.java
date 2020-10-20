b1fe49a31b6fc9c69d94c8008c2c4ae552b3e525
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-19 23:37:27 -0400

    configured checking account

--- bank/src/com/margieblair/CheckingAccount.java
+++ bank/src/com/margieblair/CheckingAccount.java
@@ -1,4 +1,8 @@
 package com.margieblair;
 
-public class CheckingAccount {
+public class CheckingAccount extends BankAccount {
+
+    public CheckingAccount(int accountNumber, int balance, String owner, String typeOfAccount) {
+        super(accountNumber, balance, owner, "Checking");
+    }
 }
