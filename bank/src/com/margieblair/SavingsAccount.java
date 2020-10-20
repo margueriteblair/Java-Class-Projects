3cdc9a1ddef02c3178ca13a33ae94c9d398c5a2c
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-19 23:40:45 -0400

    savings account implements interest method

--- bank/src/com/margieblair/SavingsAccount.java
+++ bank/src/com/margieblair/SavingsAccount.java
@@ -1,4 +1,13 @@
 package com.margieblair;
 
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount {
+    private double interest;
+
+    public SavingsAccount(int accountNumber, int balance, String owner, String typeOfAccount) {
+        super(accountNumber, balance, owner, "Savings");
+    }
+
+    public void applyInterest() {
+        balance += balance * ((float) interest / 100.0);
+    }
 }
