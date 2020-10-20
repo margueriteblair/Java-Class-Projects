2ec20491ff279049de03ee7f84e9696b75bf4efe
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-19 23:49:15 -0400

    created debit card class

--- bank/src/com/margieblair/DebitCard.java
+++ bank/src/com/margieblair/DebitCard.java
@@ -1,4 +1,20 @@
 package com.margieblair;
 
 public class DebitCard {
+    private int pin;
+    private CheckingAccount primary;
+    private int cvv;
+    private String owner;
+    private String cardNumber;
+    private List<SavingsAccount> atmAccounts;
+
+    public DebitCard(String cardNumber, int pin, int cvv, String owner, CheckingAccount primary, List atmAccounts) {
+        this.cardNumber = cardNumber;
+        this.pin = pin;
+        this.cvv = cvv;
+        this.owner = owner;
+        this.primary = primary;
+        this.atmAccounts = atmAccounts;
+    }
+
 }
--- bank/src/com/margieblair/InvestmentAccount.java
+++ bank/src/com/margieblair/InvestmentAccount.java
@@ -1,4 +1,5 @@
 package com.margieblair;
 
 public class InvestmentAccount {
+
 }
