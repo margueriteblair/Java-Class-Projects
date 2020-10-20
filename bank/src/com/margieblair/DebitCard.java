b7c5f12fdd877cf9f159dc841a6b7bae50579bab
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-20 12:42:25 -0400

    made account interface file

--- bank/src/com/margieblair/DebitCard.java
+++ bank/src/com/margieblair/DebitCard.java
@@ -2,17 +2,19 @@ package com.margieblair;
 
 public class DebitCard {
     private String pin; //ints cant handle leadings 0's
+    //we also don't need to do any math on this
     private CheckingAccount primary;
     private int cvv;
-    private String owner;
+    private String cardOwner;
     private String cardNumber;
-    private List<SavingsAccount> atmAccounts;
+    private List<Account> atmAccounts;
 
-    public DebitCard(String cardNumber, int pin, int cvv, String owner, CheckingAccount primary, List atmAccounts) {
+
+    public DebitCard(String cardNumber, int pin, int cvv, String cardOwner, CheckingAccount primary, List atmAccounts) {
         this.cardNumber = cardNumber;
         this.pin = pin;
         this.cvv = cvv;
-        this.owner = owner;
+        this.cardOwner = cardOwner;
         this.primary = primary;
         this.atmAccounts = atmAccounts;
     }
